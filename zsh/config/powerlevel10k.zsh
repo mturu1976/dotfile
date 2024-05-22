a0cd1a813af91e68fae65c5782169ad95f6ce78e
hidenori akiyama <mturu1976@gmail.com>
2024-05-22 22:03:12 +0900

    .zshrc分割

---
+++ zsh/config/powerlevel10k.zsh
@@ -0,0 +1,8 @@
+# Powerlevel10k instant prompt
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Load Powerlevel10k theme
+source ~/powerlevel10k/powerlevel10k.zsh-theme
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
