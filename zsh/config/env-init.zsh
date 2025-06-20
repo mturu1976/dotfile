8cc741edcd04e69e45dce7ba78a6fc7e31ec3b13
hidenori akiyama <mturu1976@gmail.com>
2025-06-21 00:13:09 +0900

    Tesseractに日本語対応

--- zsh/config/env-init.zsh
+++ zsh/config/env-init.zsh
@@ -17,3 +17,11 @@ export LDFLAGS="-L/usr/local/opt/tcl-tk/lib"
 export CPPFLAGS="-I/usr/local/opt/tcl-tk/include"
 export PKG_CONFIG_PATH="/usr/local/opt/tcl-tk/lib/pkgconfig"
 export PYTHON_CONFIGURE_OPTS="--with-tcltk-includes='-I/usr/local/opt/tcl-tk/include' --with-tcltk-libs='-L/usr/local/opt/tcl-tk/lib -ltcl8.6 -ltk8.6'"
+
+#Tesseractに日本語対応
+export TESSDATA_PREFIX=/opt/homebrew/share/tessdata/
+
+# Git
+export LANG=ja_JP.UTF-8
+export LC_ALL=ja_JP.UTF-8
+export GIT_PAGER="less -R"
